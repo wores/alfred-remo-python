cb85889874a184b7cc8f227048dd2e592ca98215
wores <ww.dev.publish@gmail.com>
2018-11-16 12:56:06 +0900

    rename argument name

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import inspect
 
 
-def create_instance_func_dict(clss_name):
+def create_instance_func_dict(clss):
     func_dict = {}
-    for func_name, func in inspect.getmembers(clss_name(), inspect.ismethod):
+    for func_name, func in inspect.getmembers(clss(), inspect.ismethod):
         func_dict[func_name] = func
     return func_dict
